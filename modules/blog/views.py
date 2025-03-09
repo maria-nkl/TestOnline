6692547573ccac9df3cc54dde30880187863673e
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-09 15:23:43 +0700

    Создание представления для сортировки статей по категориям/Lesson 11

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -23,3 +23,20 @@ class ArticleDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.title
         return context
+    
+
+class ArticleByCategoryListView(ListView):
+    model = Article
+    template_name = 'blog/articles_list.html'
+    context_object_name = 'articles'
+    category = None
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Article.objects.all().filter(category__slug=self.category.slug)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Статьи из категории: {self.category.title}' 
+        return context
