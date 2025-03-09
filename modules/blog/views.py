9d6bad0e1809df660115f0fa22e693e4f2a9a5c2
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-09 13:02:42 +0700

    добавление заголовка в представление/Lesson 7

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -7,3 +7,8 @@ class ArticleListView(ListView):
     model = Article
     template_name = 'blog/articles_list.html'
     context_object_name = 'articles'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
