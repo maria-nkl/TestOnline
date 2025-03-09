414b9009f7311f8951a31081baba1ae0b130a73d
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-09 14:21:57 +0700

    формирование slug в админ панели

--- modules/blog/admin.py
+++ modules/blog/admin.py
@@ -12,9 +12,11 @@ class CategoryAdmin(DraggableMPTTAdmin):
     list_display_links = ('title', 'slug')
     prepopulated_fields = {'slug': ('title',)}
 
-    fieldsets = (
-        ('Основная информация', {'fields': ('title', 'slug', 'parent')}),
-        ('Описание', {'fields': ('description',)})
-    )
+    # fieldsets = (
+    #     ('Основная информация', {'fields': ('title', 'slug', 'parent')}),
+    #     ('Описание', {'fields': ('description',)})
+    # )
 
-admin.site.register(Article)
+@admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
