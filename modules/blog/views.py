877768b268e7bc4cbb59222d3483779b79760684
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-09 13:12:34 +0700

    представление для получения одного объекта из набора/Lesson 8

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 
@@ -11,4 +11,15 @@ class ArticleListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        return context
+    
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/articles_detail.html'
+    context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
         return context
