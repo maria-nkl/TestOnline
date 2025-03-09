e647c8e098750c3148057edf873e3e7470512183
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-09 13:14:27 +0700

    добавление обработки по ссылке/Lesson 8

--- modules/blog/urls.py
+++ modules/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
+    path('articles/<str:slug>/', ArticleDetailView.as_view(), name='articles_detail'),
 ]
