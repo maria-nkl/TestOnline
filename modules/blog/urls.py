4bd878e2a41c8b03563b7db160a3459cb60b3193
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-13 13:21:43 +0700

    добавление методов CRUD для статей/Lesson 18

--- modules/blog/urls.py
+++ modules/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView, ArticleByCategoryListView
+from .views import ArticleListView, ArticleDetailView, ArticleByCategoryListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
+    path('articles/create/', ArticleCreateView.as_view(), name='articles_create'),
+    path('articles/<str:slug>/update/', ArticleUpdateView.as_view(), name='articles_update'),
+    path('articles/<str:slug>/delete/', ArticleDeleteView.as_view(), name='articles_delete'),
     path('articles/<str:slug>/', ArticleDetailView.as_view(), name='articles_detail'),
     path('category/<str:slug>/', ArticleByCategoryListView.as_view(), name="articles_by_category"),
 ]
--- modules/blog/views.py
+++ modules/blog/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Article, Category
+from .forms import ArticleCreateForm, ArticleUpdateForm
 
 
 class ArticleListView(ListView):
@@ -41,3 +43,68 @@ class ArticleByCategoryListView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Статьи из категории: {self.category.title}' 
         return context
+
+
+class ArticleCreateView(CreateView):
+    """
+    Представление: создание материалов на сайте
+    """
+    model = Article
+    template_name = 'blog/articles_create.html'
+    form_class = ArticleCreateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи на сайт'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(UpdateView):
+    """
+    Представление: обновления материала на сайте
+    """
+    model = Article
+    template_name = 'blog/articles_update.html'
+    context_object_name = 'article'
+    form_class = ArticleUpdateForm
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Обновление статьи: {self.object.title}'
+        return context
+    
+    def form_valid(self, form):
+        # form.instance.updater = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class ArticleDeleteView(DeleteView):
+    """
+    Представление: удаления материала
+    """
+    model = Article
+    success_url = reverse_lazy('home')
+    context_object_name = 'article'
+    template_name = 'blog/articles_delete.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Удаление статьи: {self.object.title}'
+        return context
+
+# from django.shortcuts import render
+# from django.core.paginator import Paginator
+
+# def articles_list(request):
+#     articles = Article.objects.all()
+#     paginator = Paginator(articles, per_page=2)
+#     page_number = request.GET.get('page')
+#     page_object = paginator.get_page(page_number)
+#     context = {'page_obj': page_object}
+#     return render(request, 'blog/articles_func_list.html', context)
