e86d30cb10f43a28f6871a50aa12ba78abf39f0a
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-17 19:45:09 +0700

    создание модели обратной связи/Lesson 28

--- modules/system/models.py
+++ modules/system/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -62,3 +63,24 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Feedback(models.Model):
+    """
+    Модель обратной связи
+    """
+    subject = models.CharField(max_length=255, verbose_name='Тема письма')
+    email = models.EmailField(max_length=255, verbose_name='Электронный адрес (email)')
+    content = models.TextField(verbose_name='Содержимое письма')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата отправки')
+    ip_address = models.GenericIPAddressField(verbose_name='IP отправителя',  blank=True, null=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
+        ordering = ['-time_create']
+        db_table = 'app_feedback'
+
+    def __str__(self):
+        return f'Вам письмо от {self.email}'
