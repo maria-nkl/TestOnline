ad1691a165af6db24d407398bd3f483dee51410a
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-17 17:19:16 +0700

    создание представления для добавления комментария/Lesson 26

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -1,12 +1,13 @@
+from django.http import JsonResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
-from .models import Article, Category
-from .forms import ArticleCreateForm, ArticleUpdateForm
+from .models import Article, Category, Comment
+from .forms import ArticleCreateForm, ArticleUpdateForm, CommentCreateForm
 from ..services.mixins import AuthorRequiredMixin
-
 
 class ArticleListView(ListView):
     model = Article
@@ -28,6 +29,7 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.title
+        context['form'] = CommentCreateForm
         return context
     
 
@@ -104,13 +106,39 @@ class ArticleDeleteView(AuthorRequiredMixin, DeleteView):
         context['title'] = f'Удаление статьи: {self.object.title}'
         return context
 
-# from django.shortcuts import render
-# from django.core.paginator import Paginator
 
-# def articles_list(request):
-#     articles = Article.objects.all()
-#     paginator = Paginator(articles, per_page=2)
-#     page_number = request.GET.get('page')
-#     page_object = paginator.get_page(page_number)
-#     context = {'page_obj': page_object}
-#     return render(request, 'blog/articles_func_list.html', context)
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentCreateForm
+
+    def is_ajax(self):
+        return self.request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    def form_invalid(self, form):
+        if self.is_ajax():
+            return JsonResponse({'error': form.errors}, status=400)
+        return super().form_invalid(form)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article_id = self.kwargs.get('pk')
+        comment.author = self.request.user
+        comment.parent_id = form.cleaned_data.get('parent')
+        comment.save()
+
+        if self.is_ajax():
+            return JsonResponse({
+                'is_child': comment.is_child_node(),
+                'id': comment.id,
+                'author': comment.author.username,
+                'parent_id': comment.parent_id,
+                'time_create': comment.time_create.strftime('%Y-%b-%d %H:%M:%S'),
+                'avatar': comment.author.profile.avatar.url,
+                'content': comment.content,
+                'get_absolute_url': comment.author.profile.get_absolute_url()
+            }, status=200)
+
+        return redirect(comment.article.get_absolute_url())
+
+    def handle_no_permission(self):
+        return JsonResponse({'error': 'Необходимо авторизоваться для добавления комментариев'}, status=400)
