3cf163c5990f0f7a1da3cce02f14122c2c2382ca
maria.nkl <nikolaevamaria2004@gmail.com>
2025-03-23 13:14:07 +0700

    debug/Lesson 39

--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,9 @@ SECRET_KEY = 'django-insecure-4)&2*wdc(zphos%gi0afs!!7o22*l%zr+l#9k)8@py!jew(*(f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
